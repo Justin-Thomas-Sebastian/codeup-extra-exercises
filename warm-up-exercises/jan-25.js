59ac1d472347d27ddf9f5950e1a4632dc68b4fbb
Justin Sebastian <justinthomassebastian@gmail.com>
2022-01-25 09:11:34 -0700

    refactored into function. updated problem description

--- warm-up-exercises/jan-25.js
+++ warm-up-exercises/jan-25.js
@@ -1,18 +1,31 @@
-// Find any duplicates in a string of numbers,
-// return an object that shows which letters where duplicated how many times
+/*
+Write a function that returns an object displaying any number/letter
+that appears consecutively in a string of numbers.
+The object should display how many times that letter/number appears consecutively
 
-let str = "BQAAIPPPPFFA";
-// let str = "42992338111134";
-let map = {};
+INPUT: “42992338111134”
+EXPECTED OUTPUT: {9: 2, 3: 2, 1: 4}
+---
+INPUT: “BQAAIPPPPFFA”
+EXPECTED OUTPUT: {a: 2, p: 4, F: 2}
+*/
 
-for(let i = 0; i < str.length - 1; i++){
-    if(str[i] === str[i + 1]){
-        if(map.hasOwnProperty(str[i])){
-            map[str[i]] += 1;
-        } else {
-            map[str[i]] = 2;
+let test1 = "42992338111134";
+let test2 = "BQAAIPPPPFFA";
+
+function countConsecutiveDuplicates(str){
+    let map = {};
+    for(let i = 0; i < str.length - 1; i++){
+        if(str[i] === str[i + 1]){
+            if(map.hasOwnProperty(str[i])){
+                map[str[i]] += 1;
+            } else {
+                map[str[i]] = 2;
+            }
         }
     }
+    return map;
 }
 
-console.log(map);
+console.log(countConsecutiveDuplicates(test1));
+console.log(countConsecutiveDuplicates(test2));
